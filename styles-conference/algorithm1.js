aef36ab93689e1091d6463ae62ffee1219352667
niuxiaopeng <648279073@qq.com>
2019-02-17 21:42:29 +0900

    学习HTML/CSS的第五天2019年2月17日21:42:22

---
+++ styles-conference/algorithm1.js
@@ -0,0 +1,29 @@
+function sumStr(i, char) {
+    let str = "";
+    let index;
+    for (index = 0; index < i; index++) {
+        str += char;
+    }
+    return str;
+}
+
+function printLine(count, num) {
+    let str = "";
+    let countHelf = Math.ceil(count / 2);
+    let numHelf = Math.ceil(num / 2);
+    let dif = countHelf - numHelf;
+    str=sumStr(dif," ")+sumStr(num,"*");
+    return str;
+}
+
+function mainFun(count){
+    let realCount=count*2-1;
+    let index;
+    for(index=0;index<count;index++)
+    {
+        console.log(printLine(realCount,index*2+1));
+    }
+}
+
+
+mainFun(20);
---
+++ styles-conference/algorithm1.test.js
@@ -0,0 +1,5 @@
+const mainFun = require('./algorithm1');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(mainFun.mainFun(20)).toBe();
+});
